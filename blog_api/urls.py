d7dc4eae7dac0e7df8236cb31938c97bc63ea0ee
Jaysal Patel <jaysalpatel.aws@gmail.com>
2021-06-22 20:06:03 -0500

    added serializer

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -4,9 +4,10 @@ from .views import PostList, PostDetail
 app_name = 'blog_api'
 
 urlpatterns = [
+    ##two endpoints showing detail post and list of views
     ##views will take in primary key
     ##second view to show all data in database
     ##one view will show one post in database
     path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
-    path('',PostList.as_view(), name='listcreate')
+    path('', PostList.as_view(), name='listcreate')
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics 
+from blog.models import Post 
+from .serializers import PostSerializer
 
 # Create your views here.
+##built in views
+class PostList(generics.ListCreateAPIView):
+    ##list all objects in database
+    queryset = Post.objects.all()
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    pass
+
