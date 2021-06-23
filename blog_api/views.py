3254e02b086b37fe69702d47762264064ccec21c
Jaysal Patel <jaysalpatel.aws@gmail.com>
2021-06-22 20:14:23 -0500

    adding serializer

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+##from django.shortcuts import render
 from rest_framework import generics 
 from blog.models import Post 
 from .serializers import PostSerializer
@@ -7,8 +7,8 @@ from .serializers import PostSerializer
 ##built in views
 class PostList(generics.ListCreateAPIView):
     ##list all objects in database
-    queryset = Post.objects.all()
-
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
 class PostDetail(generics.RetrieveDestroyAPIView):
     pass
 
